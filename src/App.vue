04c351879e3a4696b6f5c2d16d4cbd8e89f2eade
Jose Carlos Valencia Valenzuela <52218888+carlosValencia01@users.noreply.github.com>
2023-01-05 17:00:48 -0700

    v-for directive

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,24 @@
+<script setup>
+const name = 'Vue dinamico';
+const arrayFrutas = ["Apple","Banana","Melon","Strawberry","Cherry"];
+</script>
+
 <template>
   <h1>Hola Mundo Vue JS</h1>
+  <br>
+  <p>{{name}}</p>
+  <br>
+  <ul>
+    <li
+      v-for="item in arrayFrutas"
+    >
+      {{item}}
+    </li>
+  </ul>
 </template>
+    
+<style>
+h1{
+  color: red;
+}
+</style>
